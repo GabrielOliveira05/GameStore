c3d64e79c297094a0df15c7effe225b36a852cca
GabrielOliveira05 <gabrielluiz.rodrigues@hotmail.com>
2024-06-19 13:05:57 -0300

    Alteração no pom e na versão do Java que estava dando erro de versão

--- src/main/java/com/game/store/controller/JogoController.java
+++ src/main/java/com/game/store/controller/JogoController.java
@@ -1,8 +1,12 @@
 package com.game.store.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +27,7 @@ public class JogoController {
 	@Autowired
 	JogoRepository jogoRepository;
 
+	// Adicionando jogo
 	@PostMapping
 	public ResponseEntity<Jogo> salvar(@RequestBody Jogo jogo) {
 
@@ -30,6 +35,7 @@ public class JogoController {
 		return ResponseEntity.ok(jogoSalvo);
 	}
 
+	// Buscando Jogo pelo ID
 	@GetMapping("/buscarid/{id}")
 	public ResponseEntity<Jogo> getJogoById(Long id) {
 
@@ -42,6 +48,24 @@ public class JogoController {
 		return ResponseEntity.ok(jogo.get());
 	}
 
+	// Buscando Jogo pelo nome
+	@GetMapping
+	public ResponseEntity find(Jogo filtro) {
+		
+		ExampleMatcher matcher = ExampleMatcher
+												.matching()	
+												.withIgnoreCase()
+												.withStringMatcher(StringMatcher.CONTAINING);
+		
+		Example example = Example.of(filtro, matcher);
+		
+		List<Jogo> lista = jogoRepository.findAll(example);
+		
+		return ResponseEntity.ok(lista);
+	}
+	
+
+	// Deletando jogo pelo ID
 	@DeleteMapping("/deleteid/{id}")
 	public ResponseEntity<Jogo> deleteById(@PathVariable Long id) {
 		Optional<Jogo> jogo = jogoRepository.findById(id);
@@ -53,6 +77,7 @@ public class JogoController {
 		return ResponseEntity.notFound().build();
 	}
 
+	// Atualizando jogo
 	@PutMapping("/atualizar/{id}")
 	public ResponseEntity<Jogo> atualizar(@PathVariable Long id, @RequestBody Jogo jogo) {
 
@@ -63,7 +88,7 @@ public class JogoController {
 			return ResponseEntity.noContent().build();
 		});
 		return ResponseEntity.ok(jogo);
-		
+
 	}
 
 }
--- src/main/java/com/game/store/repository/JogoRepository.java
+++ src/main/java/com/game/store/repository/JogoRepository.java
@@ -1,5 +1,6 @@
 package com.game.store.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +8,6 @@ import com.game.store.model.Jogo;
 
 @Repository
 public interface JogoRepository extends JpaRepository<Jogo, Long>{
+
 
 }
